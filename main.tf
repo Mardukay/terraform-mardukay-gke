61fa66df077804a0b660e9883185dcd24b4fc812
Serhii Tsilko <mardukay@gmail.com>
2023-12-27 17:59:39 +0200

    fix variable

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 module "gke_cluster" {
-  source         = "github.com/Mardukay/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  source           = "github.com/Mardukay/tf-google-gke-cluster"
+  GOOGLE_REGION    = var.GOOGLE_REGION
+  GOOGLE_PROJECT   = var.GOOGLE_PROJECT
+  GKE_NUM_NODES    = 2
+  GKE_CLUSTER_NAME = var.GKE_CLUSTER_NAME
+  GKE_POOL_NAME    = var.GKE_POOL_NAME
 }
 
 terraform {
